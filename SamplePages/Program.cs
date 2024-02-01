802c5a50930e8884620d41ef2769419bf54d07d9
Stuart Padley <stuartpa@hotmail.com>
2024-02-01 01:38:29 -0800

    Enable SamplePages to autodetect DataApiBuilder uri

--- SamplePages/Program.cs
+++ SamplePages/Program.cs
@@ -2,13 +2,26 @@ using SamplePages.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var uri = "http://localhost:5000/graphql";
+ 
+var containerAppDnsSuffix = Environment.GetEnvironmentVariable("CONTAINER_APP_ENV_DNS_SUFFIX");
+var containerAppName = Environment.GetEnvironmentVariable("CONTAINER_APP_NAME");
+
+if (!string.IsNullOrEmpty(containerAppDnsSuffix) && !string.IsNullOrEmpty(containerAppName))
+{
+    var parts = containerAppName.Split('-');
+    containerAppName = $"{parts[0]}dataapibuilder{parts[2]}";
+
+    uri = $"https://{containerAppName}.{containerAppDnsSuffix}/graphql";
+}
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services
     .AddSamplePagesClient()
-    .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:5000/graphql"));
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri(uri));
 
 var app = builder.Build();
 
